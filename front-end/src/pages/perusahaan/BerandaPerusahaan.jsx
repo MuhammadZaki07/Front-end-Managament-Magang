19ce7d4215b74ecfb7d9c1834aa3117ae7a30a26
elisafitriana3 <nananaelisa@gmail.com>
2025-05-06 08:06:27 +0700

    Banner Cabang

--- front-end/src/pages/perusahaan/BerandaPerusahaan.jsx
+++ front-end/src/pages/perusahaan/BerandaPerusahaan.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import BannerCabang from "../../components/BannerCabang";
 import BerandaBranchCard from "../../components/cards/BerandaBranchCard";
 import PerusahaanCard from "../../components/cards/PerusahaanCard"; // Import the new component
 
 const Approval = () => {
   return (
-    <div className="p-6">
+    <div className="p-3">
       {/* Use the new PerusahaanCard component */}
+      <BannerCabang/>
       <PerusahaanCard />
 
       {/* Komponen BerandaBranchCard */}
